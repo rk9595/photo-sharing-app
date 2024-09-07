b8a171127e51b211c5a10bc797ae70d7a1abe97d
Rakesh-Kariya <rakesh.kariya@shipyaari.com>
2024-09-07 19:26:32 +0530

    home-route-added

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/rk9595/photoSharingApp
 
 go 1.23.1
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/gorilla/csrf v1.7.2
+)
+
+require github.com/gorilla/securecookie v1.1.2 // indirect
--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/rk9595/photoSharingApp/controllers"
+	"github.com/rk9595/photoSharingApp/templates"
+	"github.com/rk9595/photoSharingApp/views"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello, World</h1>")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+	r := chi.NewRouter()
+
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"home.gohtml", "tailwind.gohtml",
+	))))
 	fmt.Println("Server is running on port 8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 }
